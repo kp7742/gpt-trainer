ddd4963f99b22366df425f8b8e2d83d87661c137
kp7742 <patel.kuldip91@gmail.com>
2025-08-30 10:56:40 +0530

    Fixed Conv1D size

--- layers/conv1d.py
+++ layers/conv1d.py
@@ -11,7 +11,7 @@ class Conv1D(nn.Module):
         nn.init.normal_(self.weight, std=0.02)
 
     def forward(self, x):
-        size = x.size()[:-1]
+        size_out = x.size()[:-1] + (self.nf,)
         x = torch.addmm(self.bias, x.view(-1, x.size(-1)), self.weight)
-        x = x.view(size)
+        x = x.view(size_out)
         return x
